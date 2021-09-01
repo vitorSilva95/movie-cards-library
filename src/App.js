ee57d7b0fbab53b2dcc3d7186f6566faa366ca17
Vitor Silva <vitor.o.silva95@gmail.com>
2021-09-01 19:56:31 -0300

    Requisito 15

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
 import Data from './data';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList movies={ Data } />
     </div>
   );
