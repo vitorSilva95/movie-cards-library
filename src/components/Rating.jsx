4d94c0fa6e767c768138aed4703a06d27ed87089
Vitor Silva <vitor.o.silva95@gmail.com>
2021-09-01 18:28:41 -0300

    Requisito 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <span>{rating}</span>
+      <span className="rating">{rating}</span>
     );
   }
 }
